554313988a5e9a4ce844a9254af4aeb8e8e12cbd
nerdybond <35855776+nerdybond@users.noreply.github.com>
2021-02-01 13:55:12 +1100

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php $name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$formcontent="New website contact form message from: $name \n Message: \n $message \n";
+$recipient = "info@michaelbond.com.au";
+$subject = "Form - Hello from $name";
+$mailheader = "FROM: $email";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Sorry, but there appears to be a problem with the form you submitted. Please go back and resubmit the form.");
+
+if (isset($_POST['submit'])) 
+    ?>
+<script type="text/javascript">
+window.location = "http://www.michaelbond.com.au/";
+</script>      
+?>
